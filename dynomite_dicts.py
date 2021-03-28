3a18b74e1e71b999d8cc4f6f8d40dcc7777b1771
smelendez9 <79245515+smelendez9@users.noreply.github.com>
2021-03-27 22:58:33 -0700

    Create dynomite_dicts.py

---
+++ dynomite_dicts.py
@@ -0,0 +1,9 @@
+# In the third file, called dynomite_dicts.py do the following:
+
+# Create an empty dictionary called pokedex.
+# Add the following key, value pairs to the dictionary:
+# 'Venosaur': ['Grass', 'Poisen']
+# 'Charizard': ['Fire', 'Flying']
+# 'Blastoise': ['Water']
+
+# Remove 'Blastoise' from the dictionary.
