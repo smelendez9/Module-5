545679b862f0259fa7b4ae815bf198aca1de69ec
smelendez9 <79245515+smelendez9@users.noreply.github.com>
2021-03-27 22:56:36 -0700

    Create listy_lo.py

---
+++ listy_lo.py
@@ -0,0 +1,12 @@
+# In the second file, called listy_lo.py and do the following within the file:
+
+# Create a list named food with two elements 'rice' and 'beans'.
+# Append the string 'broccoli' to food using .append().
+# Add the strings 'bread' and 'pizza' to food using .extend().
+# Print the first two items in the food list using print() and slicing notation.
+# Print the last item in food using print() and index notation.
+# Create a list called breakfast from the string "eggs,fruit,orange juice" using the split() method.
+# Verify that breakfast has 3 elements using the len built-in.
+# prompts the user for a floating-point value until they enter stop. Store their entries in a list, and then find the average, min, and max of their entries and print them those values.
+
+if __name__ == "__main__":
