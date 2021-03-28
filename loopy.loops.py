a2be22fb267ed96a5f65566acdc500a958136f69
smelendez9 <79245515+smelendez9@users.noreply.github.com>
2021-03-27 22:54:10 -0700

    Create loopy.loops.py

---
+++ loopy.loops.py
@@ -0,0 +1,11 @@
+# Create a tuple named pokemon that holds the strings 'picachu', 'charmander', and 'bulbasaur'.
+# Using index notation, print() the string that located at index 1 in pokemon
+# Unpack the values of pokemon into the following three new variables with names starter1, starter2, starter3.
+# Create a tuple using the tuple() built-in, that contains the letters of your name.
+# Check whether the character i is in your tuple representation of your name.
+# Write a for loop that prints out the integers 2 through 10, each on a new line, by using the range() function.
+# Use a while loop that prints out the integers 2 through 10.
+# Write a for loop that iterates over the string 'This is a simple string' and prints each character.
+# Using a nested for loop, iterate over the following set ('this', 'is', 'a', 'simple', 'set') and print each word, three times.
+
+if __name__ == "__main__":
